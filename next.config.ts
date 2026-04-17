7f2f19855570d183364fc402c78985c5d589c6e5
TECH-0002 <sarobidyf036@gmail.com>
2026-04-17 11:20:51 +0300

    commit2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',      // <--- AJOUTEZ CETTE LIGNE
   images: {
     remotePatterns: [],
-    unoptimized: false,
+    unoptimized: true,   // <--- CHANGEZ EN 'true'
   },
 };
 
