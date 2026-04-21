916e1a3ea4ffd5f62556ec64ae43c6ebe631a5b3
TECH-0002 <sarobidyf036@gmail.com>
2026-04-21 15:29:57 +0300

    update4

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // AJOUTE CETTE LIGNE : C'est elle qui crée le dossier 'out'
+  output: 'export', 
+  
   images: {
     remotePatterns: [],
     unoptimized: true,
   },
   compress: true,
   poweredByHeader: false,
+  
+  // Optionnel : Si tu déploies dans un sous-dossier (ex: /karibotel/), 
+  // décommente la ligne ci-dessous et ajuste le chemin
+  // basePath: '/karibotel',
 };
 
 export default nextConfig;
